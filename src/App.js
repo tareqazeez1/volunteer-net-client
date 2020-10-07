0b0afad75f2829c14fbc0d5122520163cd30e022
tareqazeez1 <tareqazeez1@gmail.com>
2020-10-08 05:41:02 +0600

    client side almost done

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Login from './Components/Login/Login';
 import Register from './Components/Register/Register';
 import PrivateRoute from './Components/Private/PrivateRoute';
 import Registration from './Components/Registration/Registration';
-import Task from './Components/Task/Task';
+
 
 
 export const UserContext = createContext();
